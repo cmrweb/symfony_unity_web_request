0790811c73b5c89943a526f7b32666ef9f8183f2
cmrweb <tykrastayt@gmail.com>
2024-07-28 16:25:37 +0200

    user

--- src/Controller/UnityRequestController.php
+++ src/Controller/UnityRequestController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Security\AuthAuthenticator;
 use App\Service\UnityRequestHandlerService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,8 +21,14 @@ class UnityRequestController extends AbstractController
     }
 
     #[Route('/unity/request', name: 'app_unity_request')]
-    public function request(Request $request, UnityRequestHandlerService $unityRequestHandler): Response
+    public function request(Request $request, UnityRequestHandlerService $unityRequestHandler, Security $security): Response
     {
-        return $unityRequestHandler->handleRequest($request);
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        if(null === $user) {
+            return $unityRequestHandler->handleRequest($request);
+        }
+        $security->login($user, AuthAuthenticator::class);
+        return new JsonResponse(['Data' =>['username' => $user->getUsername() , 'password' => 'Already connected']]);
     }
 }
--- src/Service/UnityRequestHandlerService.php
+++ src/Service/UnityRequestHandlerService.php
@@ -1,22 +1,51 @@
 <?php
 namespace App\Service;
 
-use Cmrweb\UnityWebRequest\UnityRequest;
+use App\Entity\User;
+use App\Security\AuthAuthenticator;
+use Cmrweb\UnityWebRequest\UnityRequest; 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface; 
 
 class UnityRequestHandlerService extends UnityRequest
 { 
+    public function  __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly Security $security,
+        private readonly ParameterBagInterface $param
+    )
+    {
+            parent::__construct($param);
+    }
     public function handleSuccess(object $data): JsonResponse
     {
-        ###> do something ###
-        if(!isset($data->username)) {
-            return $this->handleError('invalid username is missing');
+        ###> do something ### 
+        if(!isset($data->username) || !isset($data->password)) {
+            return $this->handleError('invalid request missing data');
         }
-        if(empty($data->username)) {
-            return $this->handleError('username cannot be empty'); 
+        if(empty($data->username) || empty($data->password)) {
+            return $this->handleError('fields cannot be empty');
         }
-        // $unityTest = $this->serializer->deserialize($data, UnityTest::class, 'json');
-        $data->password = 'data added from server';
+
+        $user = $this->em->getRepository(User::class)->findOneBy(['username' => $data->username]);
+        if(null === $user) {
+            $user = (new User())->setUsername($data->username);
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $data->password
+            );
+            $user->setPassword($hashedPassword);
+    
+            $this->em->persist($user);
+            $this->em->flush();
+            $data->password = "Created";
+        }
+        $data->password = "Connected";
+        $this->security->login($user, AuthAuthenticator::class);
         ###< do something ### 
         return new JsonResponse(['Data'=> $data]);
     }
@@ -25,4 +54,5 @@ class UnityRequestHandlerService extends UnityRequest
     {
         return new JsonResponse(['Error' => $message]);
     }
+
 }
